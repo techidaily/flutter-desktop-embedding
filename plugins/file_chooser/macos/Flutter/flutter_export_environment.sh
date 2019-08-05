438e5be633982b9ecbb1d89ae1c6b9a4d46caebf
sunzhifeng <sunzhifeng@woda.ink>
2019-08-05 14:50:03 +0800

    构建生成的环境

---
+++ plugins/file_chooser/macos/Flutter/flutter_export_environment.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\github\flutter-stack\flutter-desktop-embedding\plugins\file_chooser"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_FRAMEWORK_DIR=D:\flutter\bin\cache\artifacts\engine\darwin-x64"
+export "FLUTTER_BUILD_NAME=0.0.1"
